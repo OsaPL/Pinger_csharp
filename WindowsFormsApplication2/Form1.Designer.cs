5f0619cb52083e4ee79802a0fb94ec1489c4b0bf
OsaPL <osa12pl@gmail.com>
2017-08-31 13:53:11 +0200

    Added prototype to test different apps

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 82);
+            this.textBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 100);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 121);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(197, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.GetActiveProcessesConnections);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(12, 227);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(179, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using IPHelper;
+using System.Diagnostics;
+using System.Threading;
+using System.Net.NetworkInformation;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out uint ProcessId);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        string GetActiveProcessFileName()
+        {
+            IntPtr hwnd = GetForegroundWindow();
+            uint pid;
+            GetWindowThreadProcessId(hwnd, out pid);
+            Process p = Process.GetProcessById((int)pid);
+            return p.MainModule.FileName;
+        }
+        uint GetActiveProcessId()
+        {
+            IntPtr hwnd = GetForegroundWindow();
+            uint pid;
+            GetWindowThreadProcessId(hwnd, out pid);
+            return pid;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            button1.PerformClick();
+
+            Thread thread = new Thread(new ThreadStart(SendPing));
+            thread.Start();
+        }
+
+        private void SendPing()
+        {
+            if (ip != String.Empty)
+            {
+                Ping pingClass = new Ping();
+                PingReply pingReply = pingClass.Send(ip);
+                button1.Text = pingReply.RoundtripTime.ToString();
+                if (pingReply.Status != IPStatus.Success)
+                {
+                    ip = String.Empty;
+                }
+            }
+        }
+        string ip = String.Empty;
+        int lastId;
+        private void GetActiveProcessesConnections(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = GetActiveProcessFileName();
+
+                uint id = GetActiveProcessId();
+                if (id != lastId)
+                {
+                    ip = String.Empty;
+                    lastId = (int)id;
+                    button1.Text = String.Empty;
+                    IpCounting.Clear();
+                }
+
+                textBox1.Text = name + "\r\nID:" + id;
+                textBox2.Clear();
+
+                var tcpArray = Functions.GetExtendedTcpTable(true, Win32Funcs.TcpTableType.OwnerPidAll).ToList();
+                listBox1.Items.Clear();
+                foreach (TcpRow tcp in tcpArray)
+                {
+                    if (tcp.ProcessId == id && !tcp.RemoteEndPoint.ToString().Contains("127.0.0.1") && !tcp.RemoteEndPoint.ToString().Contains("0.0.0.0"))
+                    {
+                        listBox1.Items.Add(tcp);
+
+                        int max = 0;
+                        string maxip = "";
+                        foreach (IPcounter ip in IpCounting)
+                        {
+                            if (ip.Count > max)
+                            {
+                                max = ip.Count;
+                                maxip = ip.Ip;
+                            }
+                        }
+                        if(max > 200)
+                        {
+                            ip = maxip;
+                        }
+
+                        textBox1.Text += "\r\n" + ip + " Count:" + max;
+
+                        textBox2.Text = ip;
+                    }
+                }
+            }
+            catch (Exception er)
+            {
+                textBox2.Text = er.ToString();
+            }
+        }
+        List<IPcounter> IpCounting = new List<IPcounter>();
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count >= 1)
+            {
+                foreach (TcpRow tcp in listBox1.Items)
+                {
+                    string temp = tcp.RemoteEndPoint.ToString().Substring(
+                                    0, tcp.RemoteEndPoint.ToString().IndexOf(":"));
+                    bool found = false;
+                    if (IpCounting.Count > 1)
+                    {
+                        foreach (IPcounter ip in IpCounting)
+                        {
+                            if (ip.Ip == temp)
+                            {
+                                ip.Count++;
+                                found = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!found)
+                    {
+                        IpCounting.Add(new IPcounter(temp));
+                    }
+                }
+            }
+        }
+    }
+    public class IPcounter
+    {
+        public string Ip;
+        public int Count = 1;
+
+        public IPcounter(string Ip)
+        {
+            this.Ip = Ip;
+        }
+    }
+}
