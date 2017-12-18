8b6c04d736effdeb237cc3817cf1488c35631d10
OsaPL <osa12pl@gmail.com>
2017-12-18 12:08:21 +0100

    experimental, stay on top method

--- pinger csharp/OverlayForm.cs
+++ pinger csharp/OverlayForm.cs
@@ -159,6 +159,18 @@ namespace pinger_csharp
         }
         [DllImport("user32.dll")]
         static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+        static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+        static readonly IntPtr HWND_TOP = new IntPtr(0);
+        static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
+        const UInt32 SWP_NOSIZE = 0x0001;
+        const UInt32 SWP_NOMOVE = 0x0002;
+        const UInt32 TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+
         [DllImport("user32.dll", SetLastError = true)]
         static extern int GetWindowLong(IntPtr hWnd, int nIndex);
 
@@ -188,6 +200,8 @@ namespace pinger_csharp
             //make overlay not clickable
             int initialStyle = GetWindowLong(Handle, -20);
             SetWindowLong(Handle, -20, initialStyle | 0x80000 | 0x20);
+            //try to make it always on top, experimental
+            SetWindowPos(this.Handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
 
             string filepath = Environment.GetEnvironmentVariable("APPDATA");
             if (File.Exists(filepath + "\\Pinger\\logback.txt"))
