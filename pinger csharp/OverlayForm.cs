83d9b2658e79e9314d717f6e186cc39a8c74daf5
OsaPL <osa12pl@gmail.com>
2017-12-21 12:51:03 +0100

    Removed prototype stuff, started working on better cfg system

--- pinger csharp/OverlayForm.cs
+++ pinger csharp/OverlayForm.cs
@@ -471,47 +471,6 @@ namespace pinger_csharp
             control.Invoke((MethodInvoker)(() => field = value));
         }
 
-        #region thread_safe_methods
-        private delegate void SetPropertyThreadSafeDelegate<TResult>(
-    Control @this,
-    Expression<Func<TResult>> property,
-    TResult value);
-
-        public static void SetPropertyThreadSafe<TResult>(
-            this Control @this,
-            Expression<Func<TResult>> property,
-            TResult value)
-        {
-            var propertyInfo = (property.Body as MemberExpression).Member
-                as PropertyInfo;
-
-            if (propertyInfo == null ||
-                !@this.GetType().IsSubclassOf(propertyInfo.ReflectedType) ||
-                @this.GetType().GetProperty(
-                    propertyInfo.Name,
-                    propertyInfo.PropertyType) == null)
-            {
-                throw new ArgumentException("The lambda expression 'property' must reference a valid property on this Control.");
-            }
-
-            if (@this.InvokeRequired)
-            {
-                @this.Invoke(new SetPropertyThreadSafeDelegate<TResult>
-                (SetPropertyThreadSafe),
-                new object[] { @this, property, value });
-            }
-            else
-            {
-                @this.GetType().InvokeMember(
-                    propertyInfo.Name,
-                    BindingFlags.SetProperty,
-                    null,
-                    @this,
-                    new object[] { value });
-            }
-        }
-        #endregion
-
         private void pingthread(int id)
         {
             //add invoking methods to ensure thread safeness
@@ -1906,5 +1865,4 @@ namespace pinger_csharp
 
 
     }
-    #endregion
 }
